a984c3d179210b46fcc2874c831ee6d50f53bca5
liuyang <me@liuyangjob.cn>
2019-07-05 17:35:35 +0800

    eureka service

--- MyService/src/main/java/cn/liuyangjob/MySpringBootStarter.java
+++ MyService/src/main/java/cn/liuyangjob/MySpringBootStarter.java
@@ -3,11 +3,13 @@ package cn.liuyangjob;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
 @ComponentScan(basePackages = "cn.liuyangjob")
 @SpringBootApplication
+@EnableEurekaServer
 public class MySpringBootStarter {
     public static void main(String args[]){
         SpringApplication.run(MySpringBootStarter.class);
