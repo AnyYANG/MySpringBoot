df385e7d817d1b7ba9ac338e9145766d274799d9
liuyang <me@liuyangjob.cn>
2019-07-05 17:59:16 +0800

    mycustomer2

---
+++ MyCostomer/src/main/java/cn/liuyang/controller/EmployeeController.java
@@ -0,0 +1,21 @@
+package cn.liuyang.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by  liuyang
+ * 2019/7/5    17:53
+ * cn.liuyang.controller
+ * All Right Reserved by liuyang.
+ **/
+@RestController
+public class EmployeeController {
+
+    @RequestMapping("/getemp/{id}")
+    public String getEmploybyid(@PathVariable long id){
+
+        return null;
+    }
+}
