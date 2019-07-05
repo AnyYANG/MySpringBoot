3fba1a7279d40e82e092fa2e14dfe984879b98cc
liuyang <me@liuyangjob.cn>
2019-07-05 16:59:22 +0800

    mybatis

--- MyService/src/main/java/cn/liuyangjob/Mapper/EmployeeMapper.java
+++ MyService/src/main/java/cn/liuyangjob/Mapper/EmployeeMapper.java
@@ -1,7 +1,27 @@
 package cn.liuyangjob.Mapper;
 
-import org.apache.ibatis.annotations.Mapper;
+import cn.liuyangjob.bean.Employee;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 @Mapper
-public class EmployeeMapper {
+public interface EmployeeMapper {
+    @Select("SELECT * FROM employees WHERE emp_no =#{id}")
+    @Results(
+            id="employee",
+            value={
+            @Result(property = "empno",column = "emp_no"),
+            @Result(property = "birthdate",column = "birth_date"),
+            @Result(property = "firstname",column = "first_name"),
+            @Result(property = "lastname",column = "last_name"),
+            @Result(property = "hiredate",column = "hire_date")
+            }
+    )
+    Employee findById(@Param("id") long id);
+
+    @Select("SELECT * FROM employees")
+    @ResultMap("employee")
+    List<Employee> findAll();
+
 }
