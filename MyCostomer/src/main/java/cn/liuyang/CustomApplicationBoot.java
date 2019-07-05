15eeb3fecc4ba72dca1fa3e38101e4ad82471d86
liuyang <me@liuyangjob.cn>
2019-07-05 17:52:44 +0800

    add customer

---
+++ MyCostomer/src/main/java/cn/liuyang/CustomApplicationBoot.java
@@ -0,0 +1,23 @@
+package cn.liuyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+/**
+ * Created by  liuyang
+ * 2019/7/5    17:49
+ * cn.liuyang
+ * All Right Reserved by liuyang.
+ **/
+@ComponentScan(basePackages = "cn.liuyang")
+@SpringBootApplication
+@EnableAutoConfiguration
+@EurekaClient
+public class CustomApplicationBoot {
+    public static void main(String args[]) {
+        SpringApplication.run(CustomApplicationBoot.class);
+    }
+}
