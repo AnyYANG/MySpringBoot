2c8acbf9b8fc3abfba1cf42e6889645a72ea8e3c
liuyang <me@liuyangjob.cn>
2019-08-02 14:43:39 +0800

    feigin hystrix ok

---
+++ MyCustomerWithFeignHystrix/src/main/java/cn/liuyang/config/FeignDisableHystrixConfiguration.java
@@ -0,0 +1,11 @@
+package cn.liuyang.config;
+
+/**
+ * Created by  liuyang
+ * 2019/8/2    14:10
+ * cn.liuyang.config
+ * All Right Reserved by liuyang.
+ **/
+
+public class FeignDisableHystrixConfiguration {
+}
