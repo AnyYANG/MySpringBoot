c92c2cea21d32c16ebccd61a460ce42feb0c82b3
liuyang <me@liuyangjob.cn>
2019-08-05 17:58:04 +0800

    admin server

---
+++ myCustomerAdmin/src/main/java/cn/liuyangjob/SpringAdminApplication.java
@@ -0,0 +1,18 @@
+package cn.liuyangjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by  liuyang
+ * 2019/8/5    17:24
+ * cn.liuyangjob
+ * All Right Reserved by liuyang.
+ **/
+@SpringBootApplication
+@EnableAdminServer
+public class SpringAdminApplication {
+    public static void main(String args[]) {
+        SpringApplication.run(SpringAdminApplication.class, args);
+    }
+}
