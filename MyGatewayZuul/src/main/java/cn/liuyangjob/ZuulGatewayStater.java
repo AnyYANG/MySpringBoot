c65e6f2964acc0fe6d0ccc7f3032569bfb99cdea
刘洋 <250935102@QQ.COM>
2019-08-07 23:08:21 +0800

    zuul

--- MyGatewayZuul/src/main/java/cn/liuyangjob/ZuulGatewayStater.java
+++ MyGatewayZuul/src/main/java/cn/liuyangjob/ZuulGatewayStater.java
@@ -1,11 +1,17 @@
 package cn.liuyangjob;
 
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableEurekaClient
+@SpringBootApplication
+@EnableZuulProxy
 public class ZuulGatewayStater {
 
-    @SpringBootApplication
-    public static void main(String args[]){
-
+    public static void main(String args[]) {
+        SpringApplication.run(ZuulGatewayStater.class,args);
     }
 }
