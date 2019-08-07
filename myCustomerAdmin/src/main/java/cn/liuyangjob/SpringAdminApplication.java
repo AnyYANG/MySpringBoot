6c0eec728213fac31965235856850b1825f30e81
刘洋 <250935102@QQ.COM>
2019-08-07 21:15:16 +0800

    Revert "innerclass"  revert op
    This reverts commit 8ce402d567cf63d450db1fbc17c6d1241a6780f0.

--- myCustomerAdmin/src/main/java/cn/liuyangjob/SpringAdminApplication.java
+++ myCustomerAdmin/src/main/java/cn/liuyangjob/SpringAdminApplication.java
@@ -1,6 +1,5 @@
 package cn.liuyangjob;
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
