26b32123b84f40767b978f347df40d286bae6cba
liuyang <me@liuyangjob.cn>
2019-08-12 18:05:51 +0800

    a

--- MyGatewayZuul/src/main/java/cn/liuyangjob/controller/GatewayController.java
+++ MyGatewayZuul/src/main/java/cn/liuyangjob/controller/GatewayController.java
@@ -3,6 +3,7 @@ package cn.liuyangjob.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -21,11 +22,12 @@ public class GatewayController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @ResponseBody
     @GetMapping("/zuul/user/{id}")
     public Map<String, Object> findbyId(@PathVariable long id)
     {
         Map<String, Object> map = new HashMap<>();
-        Object user = restTemplate.getForObject("http://microservice-user/get/10001",Object.class);
+        Object user = restTemplate.getForObject("http://microservice-user/get/"+id,Object.class);
         map.put("user",user);
         Object port = restTemplate.getForObject("http://microservice-user/getport",Object.class);
         map.put("port",port);
