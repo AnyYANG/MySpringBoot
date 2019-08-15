ce460e8367dfd5911e9b243b503a67391dbb54e0
liuyang <me@liuyangjob.cn>
2019-08-15 18:46:03 +0800

    rm config server eureka support

--- MyConfigServer/src/main/java/cn/liuyangjob/MyConfigServerStarter.java
+++ MyConfigServer/src/main/java/cn/liuyangjob/MyConfigServerStarter.java
@@ -1,17 +1,16 @@
 package cn.liuyangjob;
 
-import javafx.application.Application;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 /**
  * @author liuyang
  */
 @SpringBootApplication
 @EnableConfigServer
-@EnableEurekaClient
 public class MyConfigServerStarter {
     public static void main(String args[]) {
         SpringApplication.run(MyConfigServerStarter.class,args);
